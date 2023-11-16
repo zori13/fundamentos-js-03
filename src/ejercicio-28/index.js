3e3e84e914ccc7daab01d5f62422580010e2c64b
zori13 <zori.gon@mail.com>
2023-11-16 14:46:02 +0100

    Ejercicio 28

--- src/ejercicio-28/index.js
+++ src/ejercicio-28/index.js
@@ -1,4 +1,4 @@
 // Escribe tu código aquí:
 const numeros=[1,2,23,4,5,5,56,]
 const cuadrado=numeros.map((num)=>num*num)
-console.log(cuadrado)
+console.log(cuadradogit push --set-upstream origin zori13/f3ej28)
